a3df35dd02a7fb7b5547bdf613cda4b187f4a68b
josephstephenchua <josephstephensumampongchua@gmail.com>
2024-12-12 00:50:29 +0800

    handle loading and error

--- app/src/main/java/com/example/albertsonexam/users/UsersScreen.kt
+++ app/src/main/java/com/example/albertsonexam/users/UsersScreen.kt
@@ -1,5 +1,6 @@
 package com.example.albertsonexam.users
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,12 +12,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,10 +54,18 @@ fun UsersScreen(
             viewModel.fetchUsers()
         }
 
-        UsersList(
-            modifier = Modifier.padding(paddingValues),
-            uiState.value.users
-        )
+        Box(modifier.fillMaxSize().padding(paddingValues)) {
+            if (uiState.value.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            } else if (uiState.value.errorMessage != null) {
+                Text(text = uiState.value.errorMessage ?: "Unknown error", color = Color.Red, modifier = Modifier.align(Alignment.Center).padding(16.dp))
+            } else {
+                UsersList(
+                    modifier,
+                    uiState.value.users
+                )
+            }
+        }
 
     }
 
@@ -65,7 +77,7 @@ fun UsersList(
     users: List<UserResponse>
 ) {
 
-    LazyColumn(modifier) {
+    LazyColumn(modifier.fillMaxSize()) {
         items(users) { user ->
             UserCard(user)
         }
